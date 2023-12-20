21e6affb0fc0a82215c5f484c17e77fdd392427c
adistyningrum <147791586+adistyningrum@users.noreply.github.com>
2023-12-20 23:40:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const scrollContainer = document.querySelector('.scroll-container');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
 
-nextBtn.addEventListener('click', () => {
-  scrollContainer.scrollLeft += 50; // Menggulir 50 piksel ke kanan
-});
-
-prevBtn.addEventListener('click', () => {
-  scrollContainer.scrollLeft -= 50; // Menggulir 50 piksel ke kiri
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     var navbar = document.querySelector('.navbar');
